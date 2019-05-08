84303e71ab4b2bfe864dd835fa68fe56819a6683
Ruggero Cortini <ruggero.cortini@crg.eu>
2019-05-08 11:49:10 +0200

    new: 2_make_mapper_indices.sh

---
+++ benchmark/scripts/2_make_mapper_indices.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+cd ../data
+while read line; do
+
+  # parse the input file
+  set -- $line
+  genome_url=$1
+  genome_file=${genome_url##*/}
+
+  # make the bwa index
+  bwa index $genome_file
+
+done < $genomes
+cd ../scripts
