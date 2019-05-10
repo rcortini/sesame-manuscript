4dc5b5b615ad91c517989cd3b9b69786b69f9376
Ruggero Cortini <ruggero.cortini@crg.eu>
2019-05-10 17:19:03 +0200

    2_make_genomes_index makes also my index

--- benchmark/scripts/2_make_genomes_index.sh
+++ benchmark/scripts/2_make_genomes_index.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source 0_environment.sh
+
 genomes="data/genomes.txt"
 
 while read line; do
@@ -13,6 +15,11 @@ while read line; do
   genome_file=${genome_file%%.gz}
 
   # make the bwa index
+  log_message "Making BWA index"
   bwa index $genome_file
+
+  # make my index
+  log_message "Making my index"
+  python3 scripts/make_my_genome_index.py $genome_file
 
 done < $genomes
