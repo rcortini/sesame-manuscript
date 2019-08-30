f8c50769ea8060a2d680c0db265bcbc7cfb79e47
Ruggero Cortini <ruggero.cortini@crg.eu>
2019-08-30 11:29:39 +0200

    Improvements to memtest.sh
    The scripts now writes the time at start of loop. It also exits
    correctly if it finds that the process exited in the middle of an
    internal loop.

--- benchmark/scripts/memtest.sh
+++ benchmark/scripts/memtest.sh
@@ -11,20 +11,28 @@ function checkmemory {
   outfile=$2
   myuser=$(whoami)
 
-  rm -rf $outfile
+  # initialize the file to know what time it is
+  time_now=$(date +"%s")
+  echo "$time_now 0" > $outfile
+
+  # start an infinite loop that stops when the process is over
   while [ 1 ]
   do
 
     # check that the process actually exists
     ps --pid $mainpid &> /dev/null
     if [ $? -ne 0 ]; then
-      break
+      return
     fi
 
     # put the memory footprint in file
     let memfootprint=0
     for pid in $(ps -o pid,ppid -u $myuser | grep $mainpid | awk '{ print $1 }'); do
       fp=$(ps -o rss $pid | tail -n 1)
+
+      # the process might have stopped in the meanwhile
+      if [ "$fp" -e "RSS" ] then return fi
+
       let memfootprint=fp+memfootprint
     done
     time_now=$(date +"%s")
